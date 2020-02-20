f56fafd97a538ec7b37a1a922ac672633c7cea03
davidbuzatu-marian <davidm.buzatu@gmail.com>
2020-02-20 14:23:51 +0200

    Implemented Redux Alert actions and reducers

---
+++ client/src/store.js
@@ -0,0 +1,16 @@
+// Boiler plate redux
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+const initialState = {};
+const middleware = [thunk];
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
